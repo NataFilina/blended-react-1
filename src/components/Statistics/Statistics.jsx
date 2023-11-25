0153a96d74c3b72672a8cf28490b1dfe60c356bd
uflakey <134938478+uflakey@users.noreply.github.com>
2023-11-25 13:35:59 +0200

    123

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,28 @@
 import { StatisticItem } from 'components';
 import { StatisticsList, StatisticTitle } from './Statistics.styled';
 import { FaRegThumbsUp } from 'react-icons/fa';
-import { MdPeople, MdOutlineProductionQuantityLimits } from 'react-icons/md';
+import { MdPeople, MdOutlineProductionQuantityLimits,} from 'react-icons/md';
 import { GiTreeDoor } from 'react-icons/gi';
-
-export const Statistics = () => {
+const icons = [<FaRegThumbsUp/>, <MdPeople/>, <MdOutlineProductionQuantityLimits/>, <GiTreeDoor/>]
+export const Statistics = ({ title, stats }) => {
+  
   return (
     <>
-      <div>StatisticsList</div>
+      {title&&<StatisticTitle>{title}</StatisticTitle>}
+      <StatisticsList>
+{stats.length!==0&&stats.map(({id, title, total}, indx) => (
+
+  <StatisticItem
+    key = {id}
+  title = {title}
+    total={total}
+    icon = {icons[indx]}
+  
+  />
+))}
+
+
+      </StatisticsList>
     </>
   );
 };
